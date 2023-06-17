2fb64cc518b5644b08a445397a7cfdd8d4a8ec65
CorrosiveMoon <89851845+CorrosiveMoon@users.noreply.github.com>
2023-06-18 01:07:50 +0300

    Delete Random Meme Generator directory

--- Random Meme Generator/QuoteEngine/__init__.py
+++
@@ -1,3 +0,0 @@
-"""quoteengine module responsible for ingesting many types of files that contain quotes."""
-from .quote_model import QuoteModel
-from .ingestors import Ingestor
--- Random Meme Generator/memeengine/__init__.py
+++
@@ -1,1 +0,0 @@
-from .meme_generator import MemeGenerator
